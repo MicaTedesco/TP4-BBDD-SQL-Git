f526cddec93244ef4f79eaf3568032a32895a954
MicaTedesco <163702890+MicaTedesco@users.noreply.github.com>
2025-10-22 15:19:20 -0300

    correccion query 3

--- 03_InfoMed_TP4_GramagliaTedesco.sql
+++ 03_InfoMed_TP4_GramagliaTedesco.sql
@@ -1,1 +1,1 @@
-UPDATE pacientes SET calle = 'Calle Corrientes', numero = 500, ciudad = 'Buenos Aires' WHERE nombre = 'Luciana Gómez'
+UPDATE pacientes SET calle = 'Calle Corrientes', numero = 500, ciudad = 'Buenos Aires' WHERE nombre = 'Luciana Gómez';
