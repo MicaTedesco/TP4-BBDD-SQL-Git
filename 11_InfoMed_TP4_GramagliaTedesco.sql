f447361c10b496aa8a2a42fe058195c8f17cfdfe
juanbag25 <juanbag25@gmail.com>
2025-10-26 17:44:30 -0300

    Agrego query 11

--- 11_InfoMed_TP4_GramagliaTedesco.sql
+++
@@ -1,5 +0,0 @@
-SELECT pacientes.nombre AS nombre_paciente, consultas.fecha , consultas.diagnostico
-FROM pacientes
-RIGHT JOIN consultas
-ON pacientes.id_paciente = consultas.id_paciente
-WHERE fecha BETWEEN '2024-08-01' AND '2024-08-31';
