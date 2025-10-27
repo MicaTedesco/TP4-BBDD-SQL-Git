24710e4d20762479422f525bf66ac1ca5fb0891f
MicaTedesco <163702890+MicaTedesco@users.noreply.github.com>
2025-10-27 15:20:29 -0300

    Remove redundant INNER JOIN with pacientes table

--- 18_InfoMed_TP4_GramagliaTedesco.sql
+++ 18_InfoMed_TP4_GramagliaTedesco.sql
@@ -2,17 +2,5 @@ SELECT medicos.nombre AS nombre_medico, COUNT(DISTINCT(consultas.id_paciente)) A
 FROM medicos 
 INNER JOIN consultas
 ON medicos.id_medico = consultas.id_medico
-INNER JOIN pacientes
-ON consultas.id_paciente = pacientes.id_paciente
 GROUP BY medicos.id_medico
 ORDER BY pacientes_atendidos DESC;
-
-
-
-
-
-
-
-
-
-
